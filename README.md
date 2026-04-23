eb775b03ba4d633efbeb6af2c0860c5a9f85322e
SankThomas <thomassankara74@gmail.com>
2026-04-23 09:50:29 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vite Starter Repository
+
+I am going to be using this repository for my React (JavaScript) and Vite projects.
+
+Installed are:
+1. Figtree font using Google fonts
+2. TailwindCSS
+3. React Compiler
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center text-center h-screen">
+      <article className="space-y-4">
+        <h1 className="font-bold text-4xl">Vite Starter</h1>
+        <p>This repository already has TailwindCSS installed.</p>
+      </article>
+    </div>
+  );
+}
