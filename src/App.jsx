3cec15fa5e727a263d3a45d44e87a685af48d1e9
SankThomas <thomassankara74@gmail.com>
2026-06-09 16:07:57 +0300

    update landing page text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,23 @@ export default function App() {
     <div className="flex items-center justify-center text-center h-screen">
       <article className="space-y-4">
         <h1 className="font-bold text-4xl">Vite Starter</h1>
-        <p>This repository already has TailwindCSS installed.</p>
+        <p>
+          <strong>TailwindCSS</strong> is installed.
+        </p>
+        <p>
+          This repo uses <strong>pnpm</strong>. If you are using another package
+          manager, delete
+          <br />
+          the{" "}
+          <strong>
+            <em>pnpm-lock.yaml</em>
+          </strong>{" "}
+          file before installing{" "}
+          <strong>
+            <em>node_modules</em>
+          </strong>
+          .
+        </p>
       </article>
     </div>
   );
